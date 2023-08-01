aeeeb98b39176b5932d2b9d66f6d8cdb343ab998
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-08-01 18:43:31 +0300

    Add files via upload

---
+++ ProgIntro/4k7maximum/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// int maximum() {
+int main() {
+    int list[20], a, x;
+    for (a = 0; a <= 19; a++) {
+    cin >> list[a];
+    }
+    x = list[0];
+    for (a = 0; a <= 19; a++) {
+    if (list[a] > x) x = list[a];
+    }
+    cout << x << endl;
+}
